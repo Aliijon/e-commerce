eb6d2dfecb5ef521713fec7512e82bd55d4561be
unknown <alielyasi2002@gmail.com>
2025-03-26 19:45:31 -0700

    Update Werkzeug import to use urlparse instead of url_parse

--- auth.py
+++ auth.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, request
 from flask_login import login_user, logout_user, login_required, current_user
-from werkzeug.urls import url_parse
+from werkzeug.urls import urlparse
 from models import User
 from extensions import db
 
@@ -24,7 +24,7 @@ def login():
         
         login_user(user, remember=remember)
         next_page = request.args.get('next')
-        if not next_page or url_parse(next_page).netloc != '':
+        if not next_page or urlparse(next_page).netloc != '':
             next_page = url_for('index')
         return redirect(next_page)
     
